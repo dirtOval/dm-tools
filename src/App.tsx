e1c90a09fed8be986bf1a04bfc99adaef9e42d9c
deirdre <deirdre.loyall@gmail.com>
2024-03-11 19:01:42 -0400

    nuked everything again, now building app as single window with potential to remove windows for customizable interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
+import TableManager from './components/TableManager.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <TableManager/>
+      {/* <div>
         <a href="https://electron-vite.github.io" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -22,12 +24,12 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR POTATO POTATO POTATO
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/components/TableManager.tsx
@@ -0,0 +1,9 @@
+const TableManager = () => {
+  return (
+    <div>
+      <h1>Im Table</h1>
+    </div>
+  )
+};
+
+export default TableManager;
